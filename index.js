1ee65e03f9a34b2804611be4a5a6607a14b90cac
elvis-sautet <sautetelvis4148@gmail.com>
2023-10-14 16:58:54 +0300

    feat: New button added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const container = document.getElementsByClassName("sautet")[0]
-const Header1=document.getElementById("h1")
+const container = document.getElementsByClassName("sautet")[0];
+const Header1 = document.getElementById("h1");
 
-Header1.innerText = "Anything"
-Header1.style.color = "green"
+Header1.innerText = "Anything";
+Header1.style.color = "green";
+
+// adding a button to the page
+const button = document.createElement("button"); // creating a button
+// add class to the button
+button.classList.add("btn");
+button.innerText = "Click me"; // adding text to the button
+button.style.color = "red"; // adding style to the button
+button.style.backgroundColor = "yellow"; // adding style to the button
+button.style.fontSize = "20px"; // adding style to the button
+button.style.borderRadius = "10px"; // adding style to the button
+button.style.padding = "10px";
+
+// when the button is double clicked, how a fancy alert
+button.addEventListener("dblclick", () => {
+  alert("You clicked me");
+});
+
+// mount the button to the page
+container.appendChild(button);
